1d05b4bd3419c211d2874c0602eaaae57b93f150
mdwl23 <middwland23@gmail.com>
2023-12-24 18:08:47 +0900

    graphvizかmermaidを選択できるように修正

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ class Kroki:
         self.host = host
         self.port = port
 
-    def send_data(self, text_data):
+    def send_data(self, text_data, diagram_library="graphviz"):
         # Convert text data to bytes
         data_bytes = text_data.encode("utf-8")
         # Compress data bytes with zlib
@@ -19,10 +19,13 @@ class Kroki:
         encoded_bytes = base64.urlsafe_b64encode(compressed_bytes)
         # Convert encoded bytes to string
         encoded_string = encoded_bytes.decode("utf-8")
+
         # Create URL with server address, port, diagram library, and output format
-        url = f"http://{self.host}:{self.port}/graphviz/svg/{encoded_string}"
+        url = f"http://{self.host}:{self.port}/{diagram_library}/svg/{encoded_string}"
+
         # Return URL as string
         return url
+
 
 @app.route("/")
 def index():
@@ -38,12 +41,16 @@ def index():
             function submitForm(event) {
                 // Prevent default behavior of form submission
                 event.preventDefault();
+                
                 // Create XMLHttpRequest object
                 var xhr = new XMLHttpRequest();
+                
                 // Open POST request to root path asynchronously
                 xhr.open("POST", "/", true);
+                
                 // Set request header for form data
                 xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                
                 // Define a function to handle response
                 xhr.onreadystatechange = function() {
                     // Check if ready state is 4 (done) and status is 200 (OK)
@@ -56,22 +63,33 @@ def index():
                         image.src = imageUrl;
                     }
                 };
+                
                 // Get text data from form element by name
                 var textData = document.forms[0].elements["text_data"].value;
+                
+                // Get selected diagram type
+                var diagramType = document.querySelector('input[name="diagram_type"]:checked').value;
+                
                 // Encode text data with encodeURIComponent function
                 var encodedTextData = encodeURIComponent(textData);
-                // Send request with encoded text data as parameter
-                xhr.send("text_data=" + encodedTextData);
+                
+                // Send request with encoded text data and diagram type as parameters
+                xhr.send("text_data=" + encodedTextData + "&diagram_type=" + diagramType);
             }
         </script>
     </head>
     <body>
         <h1>Dragon Generator</h1>
         <p>Graphvizのテキストデータを入力してください。</p>
-        <form onsubmit="submitForm(event)">
-            <textarea name="text_data" rows="10" cols="50">digraph G {\n  graph [fontname = "Meiryo UI"];\n  node [fontname = "Meiryo UI"];\n  edge [fontname = "Meiryo UI"];\n  Hello->World\n}</textarea>
-            <input type="submit" value="送信">
-        </form>
+            <form onsubmit="submitForm(event)">
+                <textarea name="text_data" rows="10" cols="50">digraph G {\n  graph [fontname = "Meiryo UI"];\n  node [fontname = "Meiryo UI"];\n  edge [fontname = "Meiryo UI"];\n  Hello->World\n}</textarea>
+                
+                <p>Select diagram type:</p>
+                <input type="radio" name="diagram_type" value="graphviz" checked> Graphviz
+                <input type="radio" name="diagram_type" value="mermaid"> Mermaid
+                
+                <input type="submit" value="送信">
+            </form>
         <img id="image" src="">
     </body>
     </html>
@@ -79,12 +97,21 @@ def index():
 
 @app.route("/", methods=["POST"])
 def generate():
-    # Get text data from form
+    # Get text data and diagram type from form
     text_data = request.form["text_data"]
+    diagram_type = request.form["diagram_type"]
+
     # Create Kroki instance
     kroki = Kroki(host="localhost", port=8000)
-    # kroki = Kroki(host="kroki", port=8000)
-    # Call send_data method and get image URL
-    image_url = kroki.send_data(text_data)
+
+    # Determine the diagram library and call the appropriate method
+    if diagram_type == "graphviz":
+        image_url = kroki.send_data(text_data, diagram_library="graphviz")
+    elif diagram_type == "mermaid":
+        image_url = kroki.send_data(text_data, diagram_library="mermaid")
+    else:
+        # Handle invalid diagram type
+        return jsonify({"error": "Invalid diagram type"}), 400
+
     # Return image URL as plain text
     return image_url, 200, {"Content-Type": "text/plain"}
